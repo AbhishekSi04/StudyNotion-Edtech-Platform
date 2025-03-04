27cde2d1b80aa4f3109d6dec8fcf023fd12ca506
abhi-github-159084 <abhishekabhi159084@gmail.com>
2025-03-04 08:25:23 +0530

    updated subsection model

--- server/models/SubSection.js
+++ server/models/SubSection.js
@@ -1,10 +1,26 @@
+// const mongoose = require("mongoose");
+
+// const SubSectionSchema = new mongoose.Schema({
+// 	title: { type: String },
+// 	timeDuration: { type: String },
+// 	description: { type: String },
+// 	videoUrl: { type: String },
+// });
+
+// module.exports = mongoose.model("SubSection", SubSectionSchema);
+
+
+
 const mongoose = require("mongoose");
 
-const SubSectionSchema = new mongoose.Schema({
-	title: { type: String },
-	timeDuration: { type: String },
-	description: { type: String },
-	videoUrl: { type: String },
+const subSectionSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  videoUrl: { type: String, required: true },
+  timeDuration: { type: String, required: true },
 });
 
-module.exports = mongoose.model("SubSection", SubSectionSchema);
+// Fix: Use `mongoose.models` to avoid re-compilation error
+const SubSection = mongoose.models.SubSection || mongoose.model("SubSection", subSectionSchema);
+
+module.exports = SubSection;
