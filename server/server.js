c0fbafd2b920bca6faf473f5cf60dc060a7fd1b1
abhi-github-159084 <abhishekabhi159084@gmail.com>
2025-03-05 20:37:31 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:["http://localhost:3000", "https://study-notion-edtech-platform-crfw.vercel.app/"], // Allow localhost and deployed frontend
+		origin: [process.env.CORS_ORIGIN || "http://localhost:3000"],
 		credentials:true,
 	})
 )
